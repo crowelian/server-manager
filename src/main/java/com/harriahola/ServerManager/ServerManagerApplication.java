c4628024352783328656cb0a13abe2dbdd9dd873
crowelian-w <harricgi@gmail.com>
2023-03-08 14:31:34 +0200

    Format code and add one more dummy data.

--- src/main/java/com/harriahola/ServerManager/ServerManagerApplication.java
+++ src/main/java/com/harriahola/ServerManager/ServerManagerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.harriahola.ServerManager.repository.ServerRepository;
 import org.springframework.web.cors.CorsConfiguration;
@@ -29,36 +28,37 @@ public class ServerManagerApplication {
 			serverRepository.save(
 					new Server(null, "192.168.0.0", "Fake server", "8 GB",
 							"FakeType", "http://localhost:8080/images/server1.png", Status.SERVER_UP));
+			serverRepository.save(
+					new Server(null, "54.37.72.5", "Outer fake server", "32 GB",
+							"TypeOfFake", "http://localhost:8080/images/server2.png", Status.SERVER_DOWN));
 		};
 	}
 
+	// Simple way to fix cors errors for now
+	@Bean
+	public CorsFilter corsFilter() {
+		final String angularUrlPort = "http://localhost:4200";
+		final String someOtherUrlAndPort = "http://localhost:3000";
 
-		// Simple way to fix cors errors for now
-		@Bean
-		public CorsFilter corsFilter() {
-			final String angularUrlPort = "http://localhost:4200";
-			final String someOtherUrlAndPort = "http://localhost:3000";
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList(angularUrlPort, someOtherUrlAndPort));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
 
-			CorsConfiguration corsConfiguration = new CorsConfiguration();
-			corsConfiguration.setAllowCredentials(true);
-			corsConfiguration.setAllowedOrigins(Arrays.asList(angularUrlPort, someOtherUrlAndPort));
-			corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-					"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
-					"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token",
+				"Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+				"Access-Control-Allow-Credentials",
+				"Filename"));
 
-			corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token",
-					"Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
-					"Access-Control-Allow-Credentials",
-					"Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
-			corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		CorsFilter corsFilter = new CorsFilter(urlBasedCorsConfigurationSource);
+		return corsFilter;
 
-			UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-			urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-			CorsFilter corsFilter = new CorsFilter(urlBasedCorsConfigurationSource);
-			return corsFilter;
-
-		}
-
+	}
 
 }
