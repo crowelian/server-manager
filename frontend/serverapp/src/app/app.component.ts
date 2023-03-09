15b999ddc0042ca19408a27addb97ab4a34b20bc
crowelian-w <harricgi@gmail.com>
2023-03-09 10:24:52 +0200

    filter servers on the list

--- frontend/serverapp/src/app/app.component.ts
+++ frontend/serverapp/src/app/app.component.ts
@@ -79,4 +79,20 @@ export class AppComponent implements OnInit {
   }
 
 
+  filterServers(status: Status): void {
+    console.log("FILTER SERVER with", status);
+    this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response };
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+
+
 }
