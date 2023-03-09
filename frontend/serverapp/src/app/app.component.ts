cfde00b7d2801db944a83ac023003c4e75f39960
crowelian-w <harricgi@gmail.com>
2023-03-09 08:51:40 +0200

    use the new custom pipe in the html and add a filterSubject to html to be used when pinging the server

--- frontend/serverapp/src/app/app.component.ts
+++ frontend/serverapp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, of, startWith } from 'rxjs';
 import { DataState } from './enum/data.state.enum';
+import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { ServerService } from './service/server.service';
@@ -13,6 +14,9 @@ import { ServerService } from './service/server.service';
 export class AppComponent implements OnInit {
   appState$!: Observable<AppState<CustomResponse>>;
   readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  filterStatus$: Observable<string> = this.filterSubject.asObservable();
 
   title = 'Server Manager';
   subTitle = '- remember your server -';
